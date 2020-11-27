63497da31440e6f17b3b54e44a4883471a1eb7b3
Wen30Park <72847529+Wen30Park@users.noreply.github.com>
2020-11-27 17:26:24 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+//Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+//import { AngularFireStorageModule } from 'angularfire2/';
+//import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule, 
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/carga/carga.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FileItem } from 'src/app/models/file-item';
+import { CargaImagenesService } from 'src/app/services/carga-imagenes.service';
+
+@Component({
+  selector: 'app-carga',
+  templateUrl: './carga.component.html',
+  styles: [
+  ]
+})
+export class CargaComponent implements OnInit {
+
+  archivos: FileItem[] = [];
+
+  constructor(public _cargaImagenes: CargaImagenesService) { }
+
+  ngOnInit(): void {
+  }
+
+  cargarImagenes(){
+    this._cargaImagenes.cargarImagenesFirebase(this.archivos);
+  }
+}
